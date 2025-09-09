8f7ad3b1d23cca515dd887656398e10b52893c4a
Kheely <kimberlyheely@gmail.com>
2025-09-09 11:00:46 -0400

    Created a plot looking at site and total weight.

--- scripts/03_content/assignment1.R
+++ scripts/03_content/assignment1.R
@@ -14,6 +14,11 @@ p <- ggplot(data_lionfish,
 ?geom_point
 
 view(data_lionfish)
+
 ggplot(data_lionfish,
        aes(x = size_class, y = depth_m)) +
   geom_point(colour = "blue")
+
+ggplot(data_lionfish,
+       aes(x = site, y = total_weight_gr)) +
+  geom_point(colour = "red")
